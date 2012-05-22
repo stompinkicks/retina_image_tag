1c821a3cbbab1c3c166924d548cdaead217aa44c
Holger Sindbaek <holgersindbaek@gmail.com>
2012-05-22 19:08:43 +0200

    commit to github

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -1,10 +1,32 @@
 module RetinaImageTag
   module ViewHelpers
     
-
+    def retina_image()
+      @retina_image_path = image_path(@file_name.gsub("normal", "retina")+@file_ext)
+      image_tag(@retina_image_path, @options)
+    end
     
     
-
+    def retina_image_tag(image, options = {})
+      @devicePixelRatio = cookies[:devicePixelRatio]
+      @options          = options
+      @image            = image                             # foo.jpg
+      
+      @file_path        = image_path(@image)
+      @file_ext         = File.extname(@file_path)          # .jpg, .png
+      @file_name        = File.basename(@image, @file_ext)  # pic1, foo
+      @file_dirname     = File.dirname(@file_path)
+      
+            
+      case @devicePixelRatio
+        when '2'
+          retina_image
+        when '1.5'
+          retina_image
+        else
+          image_tag(@file_path, @options)
+       end
+     end
      
   end
 end
